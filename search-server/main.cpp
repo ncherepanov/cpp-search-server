aeafc25dba5a5b99b591e39f36f1796e6ec19c26
Nikolay Cherepanov <kolya1980nv@gmail.com>
2024-04-29 08:13:37 +0300

    Финальное задание 3-го спринта

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -20,6 +20,7 @@ Nikolay Cherepanov
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+const double EPSILON = 1e-6;  // 10 в степени -6
 
 string ReadLine() {
     string s;
@@ -141,7 +142,7 @@ public:
         auto matched_documents = FindAllDocuments(query, document_predicate);
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
-                 if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                 if (abs(lhs.relevance - rhs.relevance) < EPSILON) {
                      return lhs.rating > rhs.rating;
                  } else {
                      return lhs.relevance > rhs.relevance;
